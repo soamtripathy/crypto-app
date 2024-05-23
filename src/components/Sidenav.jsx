308b7a8cd9f43de92abd9f058da3aad82938fd1f
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-05-23 23:45:06 +0530

    set up and sidenav

---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,56 @@
+import { Box, Stack, HStack, Icon, Text, Heading } from "@chakra-ui/react";
+import { RxDashboard } from "react-icons/rx";
+import { GrTransaction } from "react-icons/gr";
+import { BiSupport } from "react-icons/bi";
+
+const Sidenav = () => {
+  const navLinks = [
+    { icon: RxDashboard, text: "Dashboard", link: "/" },
+    { icon: GrTransaction, text: "Transactions", link: "/transactions" },
+  ];
+  return (
+    <Stack justify="space-between" boxShadow="lg" maxW="16rem" h="100vh">
+      <Box>
+        <Heading textAlign="center" fontSize="20px" as="h1" pt="3.5rem">
+          {" "}
+          @SOAMTRIPATHY{" "}
+        </Heading>
+        <Box mt="6" mx="3">
+          {navLinks.map((nav) => (
+            <HStack
+              borderRadius="10px"
+              mx="3"
+              key={nav.text}
+              py="3"
+              px="4"
+              _hover={{ bg: "#F3F3F7", color: "#171717" }}
+              color="#797E82"
+            >
+              <Icon as={nav.icon} />
+              <Text fontSize="14px" fontWeight="medium">
+                {nav.text}
+              </Text>
+            </HStack>
+          ))}
+        </Box>
+      </Box>
+      <Box mt="6" mx="3" mb= "6">
+        <HStack
+          borderRadius="10px"
+          mx="3"
+          py="3"
+          px="4"
+          _hover={{ bg: "#F3F3F7", color: "#171717" }}
+          color="#797E82"
+        >
+          <Icon as={BiSupport} />
+          <Text fontSize="14px" fontWeight="medium">
+            Support
+          </Text>
+        </HStack>
+      </Box>
+    </Stack>
+  );
+};
+
+export default Sidenav;
---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import Sidenav from "../../components/Sidenav"
+
+
+const Dashboard = () => {
+  return (
+    <div>
+        <Sidenav />
+    </div>
+  )
+}
+
+export default Dashboard
